5654ba139a3cbf8d6753c153f6ff60ca0f9b5dce
El1zabethS <Bestlemon555@gmail.com>
2025-09-20 15:57:22 -0700

    auth

--- assets/js/firebase_config.js
+++ assets/js/firebase_config.js
@@ -2,15 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyCsq8jy85O9JYFwACfFGmBBRWGq-jOv6Ck",
   authDomain: "fashn-devhacks.firebaseapp.com",
   projectId: "fashn-devhacks",
-  storageBucket: "fashn-devhacks.firebasestorage.app",
+  storageBucket: "fashn-devhacks.appspot.com",
   messagingSenderId: "523104349054",
   appId: "1:523104349054:web:64cede482107055da9612f",
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // Get the authentication service instance
 const auth = getAuth(app);
+
+export { app, auth };
