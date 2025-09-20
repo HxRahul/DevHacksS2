7d9ce41f24167069842932b1f5ee7cc81335e720
Elizabeth Seaton <Bestlemon555@gmail.com>
2025-09-20 15:52:04 -0700

    Feat authentication page (#8)
    * Updated nav to use index.html not home.html
    
    * firebase config
    
    * basic sign in created. Firebase not properly implemented, waiting for website published
    
    * auth pt2

---
+++ assets/js/firebase_config.js
@@ -0,0 +1,16 @@
+// assets/js/firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsq8jy85O9JYFwACfFGmBBRWGq-jOv6Ck",
+  authDomain: "fashn-devhacks.firebaseapp.com",
+  projectId: "fashn-devhacks",
+  storageBucket: "fashn-devhacks.firebasestorage.app",
+  messagingSenderId: "523104349054",
+  appId: "1:523104349054:web:64cede482107055da9612f",
+};
+
+// Get the authentication service instance
+const auth = getAuth(app);
