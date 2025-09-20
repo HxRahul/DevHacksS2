817ae4ff67bec695ef9bef375dabe827b7e30a76
El1zabethS <Bestlemon555@gmail.com>
2025-09-20 16:26:00 -0700

    Installed node

--- assets/js/firebase_config.js
+++ assets/js/firebase_config.js
@@ -1,17 +1,22 @@
 // assets/js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
-const firebaseConfig = {
+const firebaseConfig = initializeApp({
   apiKey: "AIzaSyCsq8jy85O9JYFwACfFGmBBRWGq-jOv6Ck",
   authDomain: "fashn-devhacks.firebaseapp.com",
   projectId: "fashn-devhacks",
   storageBucket: "fashn-devhacks.appspot.com",
   messagingSenderId: "523104349054",
   appId: "1:523104349054:web:64cede482107055da9612f",
-};
+});
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export { app, auth };
+const auth = getAuth(firebaseConfig);
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User is signed in:", user);
+    } else {
+    console.log("No user is signed in.");
+    }
+});
