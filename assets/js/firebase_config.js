4f1c16e7073056c623d13a311746f70ca84897b3
El1zabethS <elizabeth.seaton35@gmail.com>
2025-09-20 19:40:12 -0700

    basket works

--- assets/js/firebase_config.js
+++ assets/js/firebase_config.js
@@ -1,19 +1,24 @@
-// dashboard.js - Firebase Auth setup for browser (CDN)
+// firebase-config.js
 console.log('firebase_config.js loaded');
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCsq8jy85O9JYFwACfFGmBBRWGq-jOv6Ck",
   authDomain: "fashn-devhacks.firebaseapp.com",
   projectId: "fashn-devhacks",
-  storageBucket: "fashn-devhacks.firebasestorage.app",
+  storageBucket: "fashn-devhacks.appspot.com",
   messagingSenderId: "523104349054",
   appId: "1:523104349054:web:64cede482107055da9612f",
   measurementId: "G-NYHFZYSTT5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+// Export so other scripts can use it
+export { app, auth, db };
